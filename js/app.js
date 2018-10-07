75f64dbd10ef9946dc1d747e9480a2c36f388d10
Rena Tan <rena.tanrx@gmail.com>
2018-10-06 20:01:22 -0700

    adjusted classifcations

--- js/app.js
+++ js/app.js
@@ -3,11 +3,10 @@
 function getData(){
 //get the category from check boxes to feed to our url query string
     var stats = ($('input[name=category]:checked').val());
-    // console.log(stats);
+
 //pass in the category to the url query string
 $.ajax({
-      url: "https://data.sfgov.org/resource/nwbb-fxkq.json?incident_category=" + 'Burglary',
-	   	// url: "https://data.sfgov.org/resource/9v2m-8wqu.json?category=" + stats,
+      url: "https://data.sfgov.org/resource/nwbb-fxkq.json?incident_category=" + stats,
       type: "GET",
 	   	dataType: "json",
 //getting the data and then use the data for next step:
@@ -23,25 +22,25 @@ $.ajax({
 
 
       for(var i =0; i<data.length; i++){
-        // console.log(data[i].location.latitude);
-        // console.log(data[i].location.longitude);
-        
+        // console.log(data[i].latitude);
+        // console.log(data[i].longitude);
+
 //use parseFloat to turn the string into a number:
       var myLat = parseFloat(data[i].latitude);
       // console.log(myLat);
       var myLng = parseFloat(data[i].longitude);
       // console.log(myLng);
-      var myLatLng = {lat: myLat, lng: myLng}
+      var myLatLng = {lat: myLat, lng: myLng};
       // console.log(myLatLng);
       var marker = new google.maps.Marker({
           position: myLatLng,
           map: map,
           title: data[i].report_type_description + ' ' + data[i].incident_date + ' ' + data[i].incident_day_of_week + ' ' + data[i].intersection + ' ' + data[i].police_district
-        })
+        });
       // console.log(marker);
       }
 
     }
-	   })
+	   });
 
   }
